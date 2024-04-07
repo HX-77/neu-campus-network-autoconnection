9e6a6f80a07fcfdb3951e4dbe1fb317c63a4005e
HX-77 <804624368@qq.com>
2024-04-08 00:11:39 +0800

    create a class

--- Autoconn.py
+++ Autoconn.py
@@ -1,42 +1,58 @@
-## Main
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# 创建一个Edge浏览器实例
-driver = webdriver.Edge()
+class NEU_NET():
+    def __init__(self, url:str, username:str, password:str):
+        self.url = url
+        self.un = username
+        self.pd = password
 
-# 打开网址
-driver.get("http://ipgw.neu.edu.cn")
+    def conn(self):
+        # 创建一个Edge浏览器实例
+        driver = webdriver.Edge()
+        # 打开网址
+        driver.get(self.url) # "http://ipgw.neu.edu.cn"
 
-# 等待页面加载
-time.sleep(2)
+        # 等待页面加载
+        time.sleep(1)
 
-# 找到按钮并点击
-button = driver.find_element_by_xpath("//button[text()='连接网络']")
+        try:
+            button = driver.find_element_by_xpath("//button[text()='连接网络']")
 
-button.click()
+            button.click()
 
-# 等待页面加载
-time.sleep(2)
+            time.sleep(2)
 
-# 切换到登录页面
-driver.switch_to.window(driver.window_handles[-1])
+            # 切换到登录页面
+            driver.switch_to.window(driver.window_handles[-1])
 
-# 输入账户和密码
-username = driver.find_element_by_id("un")
-password = driver.find_element_by_id("pd")
+            # 输入账户和密码
+            username = driver.find_element_by_id("un")
+            password = driver.find_element_by_id("pd")
 
-# 填写账户和密码，这里替换成你的账户和密码
-username.send_keys("your_username")
-password.send_keys("your_password")
+            # 填写账户和密码，这里替换成你的账户和密码
+            username.send_keys(self.un)
+            password.send_keys(self.pd)
 
-# 提交登录
-password.send_keys(Keys.ENTER)
+            # 提交登录
+            password.send_keys(Keys.ENTER)
 
-# 等待登录完成
-time.sleep(2)
+            # 等待登录完成
+            time.sleep(2)
+        except:
+            time.sleep(0.5)
+            
 
-# 关闭浏览器
-driver.quit()
+        # 关闭浏览器
+        driver.quit()
+
+if __name__ == "__main__":
+    url = "http://ipgw.neu.edu.cn"
+    neu_id = "2302144"
+    neu_pd = "010220Lance"
+
+    neu_net = NEU_NET(url, neu_id, neu_pd)
+    neu_net.conn()
+
+
